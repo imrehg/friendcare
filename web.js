1c328dc15dc32c25cedcc76e6baa968aaf017c3b
Gergely Imreh <imrehg@gmail.com>
2012-04-21 16:45:01 +0800

    work version

--- web.js
+++ web.js
@@ -9,11 +9,11 @@ var app = express.createServer(
   express.bodyParser(),
   express.cookieParser(),
   // set this to a secret value to encrypt session cookies
-  express.session({ secret: process.env.SESSION_SECRET || 'secret123' }),
+  express.session({ secret: process.env.SESSION_SECRET || 'yeahthisissecretYEAH' }),
   require('faceplate').middleware({
     app_id: process.env.FACEBOOK_APP_ID,
     secret: process.env.FACEBOOK_SECRET,
-    scope:  'user_likes,user_photos,user_photo_video_tags'
+    scope:  'offline_access'
   })
 );
 
@@ -57,47 +57,79 @@ function render_page(req, res) {
 }
 
 function handle_facebook_request(req, res) {
-
   // if the user is logged in
   if (req.facebook.token) {
-
-    async.parallel([
-      function(cb) {
-        // query 4 friends and send them to the socket for this socket id
-        req.facebook.get('/me/friends', { limit: 4 }, function(friends) {
-          req.friends = friends;
-          cb();
-        });
-      },
-      function(cb) {
-        // query 16 photos and send them to the socket for this socket id
-        req.facebook.get('/me/photos', { limit: 16 }, function(photos) {
-          req.photos = photos;
-          cb();
-        });
-      },
-      function(cb) {
-        // query 4 likes and send them to the socket for this socket id
-        req.facebook.get('/me/likes', { limit: 4 }, function(likes) {
-          req.likes = likes;
-          cb();
-        });
-      },
-      function(cb) {
-        // use fql to get a list of my friends that are using this app
-        req.facebook.fql('SELECT uid, name, is_app_user, pic_square FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1', function(result) {
-          req.friends_using_app = result;
-          cb();
-        });
-      }
-    ], function() {
-      render_page(req, res);
-    });
-
+    console.log("HaveToken");
+    render_page(req, res);
   } else {
+    console.log("NotHaveToken");
     render_page(req, res);
   }
 }
+// function handle_facebook_request(req, res) {
 
-app.get('/', handle_facebook_request);
-app.post('/', handle_facebook_request);
+//   // if the user is logged in
+//   if (req.facebook.token) {
+
+//     async.parallel([
+//       function(cb) {
+//         // query 4 friends and send them to the socket for this socket id
+//         req.facebook.get('/me/friends', { limit: 4 }, function(friends) {
+//           req.friends = friends;
+//           cb();
+//         });
+//       },
+//       function(cb) {
+//         // query 16 photos and send them to the socket for this socket id
+//         req.facebook.get('/me/photos', { limit: 16 }, function(photos) {
+//           req.photos = photos;
+//           cb();
+//         });
+//       },
+//       function(cb) {
+//         // query 4 likes and send them to the socket for this socket id
+//         req.facebook.get('/me/likes', { limit: 4 }, function(likes) {
+//           req.likes = likes;
+//           cb();
+//         });
+//       },
+//       function(cb) {//         // use fql to get a list of my friends that are using this app
+//         req.facebook.fql('SELECT uid, name, is_app_user, pic_square FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1', function(result) {
+//           req.friends_using_app = result;
+//           cb();
+//         });
+//       }
+//     ], function() {
+//       render_page(req, res);
+//     });
+
+//   } else {
+//     render_page(req, res);
+//   }
+// }
+
+// app.get('/', handle_facebook_request);
+// app.post('/', handle_facebook_request);
+
+app.get('/', function(request, response) {
+
+       render_page(request, response);
+        // // render the home page
+        // response.render('index.ejs', {
+        //   layout:   false,
+        //   user: 'Greg',
+        //   app:      app
+        // });
+
+});
+
+// respond to GET /home
+app.get('/timeline', function(request, response) {
+
+        // render the home page
+        response.render('timeline.ejs', {
+          layout:   false,
+          app:      app
+        });
+
+});
