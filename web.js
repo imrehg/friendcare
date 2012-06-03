e110f3219d289aa33297109da6069b8257fb0f47
Gergely Imreh <imrehg@gmail.com>
2012-06-04 02:05:13 +0800

    some working Facebook login version

--- web.js
+++ web.js
@@ -2,175 +2,80 @@
 var express = require('express')
   , util = require('util')
   , everyauth = require('everyauth')
-  , facebook = require('facebook')
   , ejs = require('ejs')
   ;
 
+var appID = process.env.FACEBOOK_APP_ID;
 
 var app = express.createServer(
-  express.logger(),
-  express.static(__dirname + '/public'),
-  express.bodyParser(),
-  express.cookieParser(),
-  // set this to a secret value to encrypt session cookies
-  express.session({ secret: process.env.SESSION_SECRET || 'blabfldfasbvfasevfcdvfas' }),
-  function(request, response, next) {
-    var method = request.headers['x-forwarded-proto'] || 'http';
-    everyauth.facebook.myHostname(method + '://' + request.headers.host);
-    next();
-  },
-  everyauth.middleware(),
-  require('facebook').Facebook()
+    express.logger()
+  , express.static(__dirname + '/public')
+  , express.bodyParser()
+  , express.cookieParser()
+  , express.favicon()
+  , everyauth.middleware()
+  , express.session({secret: process.env.SESSION_SECRET || 'kdgfcbdgfsgftsrcgsgr'})
+  , express.errorHandler()
 );
+everyauth.helpExpress(app);
+everyauth.debug = true;
 
-app.get('/', function(req, res) {
-  // if (req.session.auth) {
-  //     console.log(req.session);
-  //     res.redirect('/dash');
-  // }
-  // else {
-  //   res.render('opening.jade', {
-  // 	           title: "Opening"
-  //             });	    
-  // }
+var usersById = {};
+var usersByFbId = {};
+var nextUserId = {};
 
-     res.render('front.ejs', {
-		    title: "FriendCare"
-		});
+everyauth.facebook
+  .appId(process.env.FACEBOOK_APP_ID)
+  .appSecret(process.env.FACEBOOK_SECRET)
+  .findOrCreateUser(function(session, accessToken, accessTokExtra, fbUserMetadata) {
+    console.log(accessToken, accessTokExtra);
+    var expire = new Date();
+    expire.setTime(expire.getTime()+accessTokExtra.expires*1000);
+    console.log("Expires:", expire);
+    return usersByFbId[fbUserMetadata.id] || (usersByFbId[fbUserMetadata.id] = addUser('facebook', fbUserMetadata));
+   })
+  .redirectPath('/dash');
+
+function addUser(source, sourceUser) {
+    var user;
+    if (arguments.length === 1) { // password-based
+        user = sourceUser = source;
+        user.id = ++nextUserId;
+        return usersById[nextUserId] = user;
+    } else { // non-password-based
+        user = usersById[++nextUserId] = {
+            id: nextUserId
+        };
+        user[source] = sourceUser;
+    }
+    return user;
+}
+everyauth.debug = true;
+
+app.configure(function() {
+    app.use(everyauth.middleware());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    everyauth.helpExpress(app);
 });
 
+app.get("/", function (req, res) {
+  res.render('front.ejs', {
+                 title: "Welcome to Friendcare",
+		 appID: appID
+		 });
+});
+
+app.get("/dash", function (req, res) {
+  // console.log(util.inspect(everyauth.facebook));
+  // console.log(util.inspect(everyauth.facebook.user));
+  res.render('dash.ejs', {
+		 title: "Friendcare"
+	     });
+});
 
 var port = process.env.PORT || 3000;
 
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-// var async   = require('async');
-// var express = require('express');
-// var util    = require('util');
-// var jade = require('jade');
-// var everyauth = require('everyauth');
-// var FacebookClient = require('facebook-client').FacebookClient;
-// var facebook = new FacebookClient();
-
-// var mongoose = require('mongoose');
-// var mongouser = process.env.MONGOUSER;
-// var mongopass = process.env.MONGOPASS;
-// mongoose.connect('mongodb://'+mongouser+':'+mongopass+'@ds033067.mongolab.com:33067/friendcare');
-
-// var Schema = mongoose.Schema
-//   , ObjectId = Schema.ObjectId;
-
-// // Schemas
-// var Update = new Schema({
-//     date : Date
-//   , fgained : [String]
-//   , flost : [String]
-// });
-
-// var User = new Schema({
-//     user      : ObjectId
-//   , fbid      : String
-//   , fbtoken   : String
-//   , friendlist: [String]
-//   , lastupdate: Date
-//   , updates   : [Update]
-// });
-
-// // configure facebook authentication
-// everyauth.facebook
-//   .appId(process.env.FACEBOOK_APP_ID)
-//   .appSecret(process.env.FACEBOOK_SECRET)
-//   .scope('offline_access')
-//   .entryPath('/login')
-//   .redirectPath('/home')
-//   .findOrCreateUser(function() {
-//     return({});
-//   });
-
-
-// // create an express webserver
-// var app = express.createServer(
-//   express.logger(),
-//   express.static(__dirname + '/public'),
-//   express.bodyParser(),
-//   express.cookieParser(),
-//   // set this to a secret value to encrypt session cookies
-//   express.session({ secret: process.env.SESSION_SECRET || 'yeahthisissecretYEAH' }),
-//   function(request, response, next) {
-//     var method = request.headers['x-forwarded-proto'] || 'http';
-//     everyauth.facebook.myHostname(method + '://' + request.headers.host);
-//     next();
-//   },
-//   everyauth.middleware(),
-//   require('facebook').Facebook()
-// );
-
-// app.set('view options', {
-//   layout: false
-// });
-
-// // listen to the PORT given to us in the environment
-// var port = process.env.PORT || 3000;
-
-// app.listen(port, function() {
-//   console.log("Listening on " + port);
-// });
-
-// app.get('/', function(req, res) {
-//   if (req.session.auth) {
-//       console.log(req.session);
-//       res.redirect('/dash');
-//   }
-//   else {
-//     res.render('opening.jade', {
-// 	           title: "Opening"
-//               });	    
-//   }
-// });
-
-// app.get('/home', function(req, res) {
-//   res.redirect('/dash');
-// });
-// app.get('/dash', function(req, res) {
-//   if (req.session.auth) {
-//       res.render('dash.jade', {
-// 	             title: "Dashboard",
-//   		     token: req.session.auth.facebook.accessToken
-//       });      
-//   } else {
-//       res.redirect('/login');
-//   }
-// });
-
-
-// /////////////////////////////////
-// app.get('/jade', function(req, res) {
-//     console.log(req.facebook.me);
-//     res.render('test.jade', {
-// 	           title: "Testing",
-//                    loggedin: req.facebook.token
-//               });
-// });
-
-
-// // respond to GET /home
-// app.get('/timeline', function(request, response) {
-
-//         // render the home page
-//         response.render('timeline.ejs', {
-//           layout:   false,
-//           app:      app
-//         });
-
-// });
-
-// // Catch 404
-// app.get('*', function(request, response) {
-// 	    lookingfor = request.params[0];
-// 	    response.render('404.jade', {
-// 				layout:   false,
-// 				lookingfor: lookingfor
-// 			    });
-// });
