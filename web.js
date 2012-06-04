0289739cf968a03b15e689a2d3e0ca22c8f3089a
Gergely Imreh <imrehg@gmail.com>
2012-06-04 13:34:55 +0800

    basic database adding of a single user

--- web.js
+++ web.js
@@ -3,9 +3,14 @@ var express = require('express')
   , util = require('util')
   , everyauth = require('everyauth')
   , ejs = require('ejs')
+  , graph = require('fbgraph')
+  , mongoose = require('mongoose')
   ;
 
-var appID = process.env.FACEBOOK_APP_ID;
+var appID = process.env.FACEBOOK_APP_ID
+  , Schema = mongoose.Schema
+  , ObjectId = Schema.ObjectId
+  ;
 
 var app = express.createServer(
     express.logger()
@@ -24,15 +29,53 @@ var usersById = {};
 var usersByFbId = {};
 var nextUserId = {};
 
+var ChangeSchema = new Schema({
+    date: {type: Date, default: Date.now},
+    gain: [String],
+    loss: [String]
+});
+
+var PersonSchema = new Schema({
+    person : ObjectId,
+    facebook: {
+        userid: String,
+	authtoken: {type: String},
+	authexpire: {type: Date},
+	friendlist: [String],
+	lastcheck: {type: Date, default: Date.now},
+	changes: [ChangeSchema]
+    }
+});
+
+mongoose.connect('mongodb://'+process.env.MONGO_USER+':'+process.env.MONGO_PASS+'@'+process.env.MONGO_URL);
+var PersonModel = mongoose.model('Person', PersonSchema);
+
+
 everyauth.facebook
   .appId(process.env.FACEBOOK_APP_ID)
   .appSecret(process.env.FACEBOOK_SECRET)
   .findOrCreateUser(function(session, accessToken, accessTokExtra, fbUserMetadata) {
-    // console.log(accessToken, accessTokExtra);
-    // var expire = new Date();
-    // expire.setTime(expire.getTime()+accessTokExtra.expires*1000);
-    // console.log("Expires:", expire);
-    return usersByFbId[fbUserMetadata.id] || (usersByFbId[fbUserMetadata.id] = addUser('facebook', fbUserMetadata));
+      var promise = this.Promise();
+      PersonModel.findOne({"facebook.userid" : fbUserMetadata.id}, function(err, user) {
+	  if (err) {
+	      return promise.fail(err);
+	  } else if (!user) {
+	      console.log("User creation");
+	      var newperson = new PersonModel();
+	      var expire = new Date();
+	      expire.setTime(expire.getTime()+accessTokExtra.expires*1000);
+	      newperson.facebook.userid = fbUserMetadata.id;
+	      newperson.facebook.authtoken = accessToken;
+	      newperson.facebook.authexpire = expire;
+	      newperson.facebook.friendlist = [];
+	      newperson.save();
+	      return promise.fulfill(usersByFbId[fbUserMetadata.id] || (usersByFbId[fbUserMetadata.id] = addUser('facebook', fbUserMetadata)));
+	  } else {
+	      console.log("User exists");
+	      return promise.fulfill(usersByFbId[fbUserMetadata.id] || (usersByFbId[fbUserMetadata.id] = addUser('facebook', fbUserMetadata)));
+	  }
+      });
+      return promise;
    })
   .redirectPath('/dash');
 
@@ -44,7 +87,7 @@ function addUser(source, sourceUser) {
         return usersById[nextUserId] = user;
     } else { // non-password-based
         user = usersById[++nextUserId] = {
-            id: nextUserId
+           id: nextUserId
         };
         user[source] = sourceUser;
     }
